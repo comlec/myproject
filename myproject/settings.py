706bebe4234f01edf763dc2d1e2d619391224355
comlec <comlec0703@gmail.com>
2025-08-25 12:11:22 +0900

    allowed_hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY', 'secretkey')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # .env에 ALLOWED_HOSTS=3.15.21.1,domain.com  
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1').split(',')
 
 # Application definition
 
